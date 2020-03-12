606b39707763b491325dd2dc31090d199650bde8
ephantus_dev <ephantokumu98@gmail.com>
2020-03-12 18:58:22 +0300

    adding dummy data

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -5,6 +5,7 @@ import SignedOutLinks from './SignedOutLinks'
 
 const Navbar = () => {
     return (
+
         <nav className="nav-wrapper grey darken-3">
             <div className="container">
                 <Link to='/' className="brand-logo">
--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ProjectSummary from './ProjectSummary';
 
- const ProjectList = ()=> {
+ const ProjectList = ({projects})=> {
   return (
       <div className="project-list section">
-         <ProjectSummary />
-         <ProjectSummary />
-         <ProjectSummary />
-         <ProjectSummary />
-       
-    
-      </div>
+       {
+         projects && projects.map(project =>{
+           return(
+             <ProjectSummary project={project} key={project.id} />
+           )
 
+         })
+       }
+       
+      </div>
   );
 }
 
