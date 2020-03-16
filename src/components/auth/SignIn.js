d98a0b97df0e37c1c25d3dff7eea3a2b788dd00a
ephantus_dev <ephantokumu98@gmail.com>
2020-03-16 08:05:15 +0300

    sign-in

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import {connect} from 'react-redux'
+import {signIn} from '../../store/actions/authactions'
 
  class SignIn extends Component {
      state = {
@@ -14,10 +16,12 @@ import React, { Component } from 'react'
 
      handleSubmit = (e) =>{
          e.preventDefault();
-         console.log(this.state)
+         //console.log(this.state)
+         this.props.signIn(this.state)
      }
     
     render() {
+        const {authError} = this.props;
         return (
             <div className="container">
                 <form onSubmit={this.handleSubmit} className="white">
@@ -31,13 +35,29 @@ import React, { Component } from 'react'
                         <input type="password" id="password" onChange={this.handleChange} />
                     </div>
                     <div className="input-field">
-                        <button className="btn pink lighten-1 z-depth-0">Login </button>
+                        <button className="btn pink lighten-1 z-depth-0">Login </button
+                        >
+                        <div className="red-text center">
+                        {authError ? <p>{authError}</p>:null}
+
+                        </div>
                     </div>
+
 
                 </form> 
                  </div>
         )
     }
 }
+const mapStateToProps = (state) =>{
+    return{
+        authError: state.auth.authError
+    }
+}
+const mapDispatchToProps = (dispatch) =>{
+    return{
+        signIn: (creds) =>dispatch(signIn(creds))
+    }
+}
 
-export default SignIn
+export default connect(mapStateToProps,mapDispatchToProps)(SignIn)
